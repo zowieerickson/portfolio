80bf0d9f9a8c6494f5709d2f4d8e59d038775de1
Zowie Erickson <zowie.erickson@gmail.com>
2026-01-25 21:30:55 -0800

    add particles to background

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,62 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Map from "./components/Map/Map";
 import Headshot from "./components/Headshot/Headshot";
+import Particles from "@tsparticles/react";
+import { useCallback } from "react";
+import { loadFull } from "tsparticles";
+import { ParticlesComponent } from "./components/Particles.tsx";
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <Map />
-      <div className="flex gap-5">
-        <Headshot />
-        <div>
-          <h2 className="font-semibold text-xl tracking-tighter sm:text-2xl">
-            Hey, I'm Zowie (zau•ee) 👋
-          </h2>
-          <a
-            href="mailto:zowie.erickson@gmail.com"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="flex items-center gap-1.5"
-          >
-            <div className="size-2 rounded-full bg-green-500"></div>
-            <div className="relative cursor-pointer overflow-hidden">
-              <p className="group text-muted-foreground">
-                <span className="group-hover:-translate-y-full flex flex-col border-b border-dashed transition-all duration-1000 ease-slow">
-                  Available for work
-                  <span className="invisible h-0"> Reach out</span>
-                </span>
-                <span className="group-hover:-translate-y-full absolute top-full flex items-center justify-center transition-all duration-1000 ease-slow">
-                  Reach out
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="ml-1 size-4"
-                  >
-                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                  </svg>
-                </span>
-              </p>
-            </div>
-          </a>
+    <>
+      <ParticlesComponent />
+      <main className="relative">
+        <Navbar />
+        <Map />
+        <div className="flex gap-5">
+          <Headshot />
+          <div>
+            <h2 className="font-semibold text-xl tracking-tighter sm:text-2xl">
+              Hey, I'm Zowie (zau•ee) 👋
+            </h2>
+            <a
+              href="mailto:zowie.erickson@gmail.com"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="flex items-center gap-1.5"
+            >
+              <div className="size-2 rounded-full bg-green-500"></div>
+              <div className="relative cursor-pointer overflow-hidden">
+                <p className="group text-muted-foreground">
+                  <span className="group-hover:-translate-y-full flex flex-col border-b border-dashed transition-all duration-1000 ease-slow">
+                    Available for work
+                    <span className="invisible h-0"> Reach out</span>
+                  </span>
+                  <span className="group-hover:-translate-y-full absolute top-full flex items-center justify-center transition-all duration-1000 ease-slow">
+                    Reach out
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="ml-1 size-4"
+                    >
+                      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                    </svg>
+                  </span>
+                </p>
+              </div>
+            </a>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Particles.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+
+export const ParticlesComponent = () => {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: "#000",
+      },
+      detectRetina: false,
+      fpsLimit: 120,
+      interactivity: {
+        detectsOn: "canvas",
+        events: {
+          resize: true,
+        },
+      },
+      particles: {
+        color: {
+          value: "#fff",
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 1080,
+          },
+          limit: 0,
+          value: 400,
+        },
+        opacity: {
+          animation: {
+            enable: true,
+            minimumValue: 0.05,
+            speed: 0.25,
+            sync: false,
+          },
+          random: {
+            enable: true,
+            minimumValue: 0.05,
+          },
+          value: 1,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          random: {
+            enable: true,
+            minimumValue: 0.5,
+          },
+          value: 0.5,
+        },
+      },
+    }),
+    [],
+  );
+
+  return (
+    <Particles
+      id="tsparticles"
+      particlesLoaded={particlesLoaded}
+      options={options}
+    />
+  );
+
+  return <></>;
+};
