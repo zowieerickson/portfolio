6f91a8eda1e0935b152f420fc61b09db70c46dff
Zowie Erickson <zowie.erickson@gmail.com>
2026-01-31 12:31:05 -0800

    reorganize Job component layout

--- src/sections/WorkSection/Job.tsx
+++ src/sections/WorkSection/Job.tsx
@@ -15,9 +15,17 @@ export default function Job({
 }: Job) {
   return (
     <div className=" p-5 w-full border rounded-lg">
-      <h1 className="font-semibold text-2xl capitalize">{position}</h1>
-      <p className="font-normal mt-2">{location}</p>
-      <p className="font-normal mt-1">{company}</p>
+      <header className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-2xl capitalize">{position}</h1>
+          <p className="font-normal">{company}</p>
+        </div>
+        <div className="flex flex-col items-end">
+          <p className="font-normal">{dates}</p>
+          <p className="font-normal">{location}</p>
+        </div>
+      </header>
+
       <ul className="mt-4 font-normal text-gray-700 list-disc list-outside dark:text-white/75">
         {description.map((item) => (
           <li className="ms-3.5" key={item}>
@@ -25,7 +33,6 @@ export default function Job({
           </li>
         ))}
       </ul>
-      <p className="font-normal mt-5">{dates}</p>
     </div>
   );
 }
